96a07c48e3d4a5637a4d146a634ae2b6114f20ba
Winsen <loren.winsen@gmail.com>
2024-03-20 14:04:21 +0700

    initial commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by winsen on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
